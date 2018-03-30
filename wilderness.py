1bad1c1d6bc319e683ef026e3b256d5aeef265fd
Pedro Leitao <pcleitao391@gmail.com>
2018-03-30 22:54:22 +0100

    Improvement of the actions menu and addition of items to inventory

--- wilderness.py
+++ wilderness.py
@@ -6,14 +6,14 @@ Created on Mon Mar 19 15:46:16 2018
 """
 #Lists of objects
 directions_options = {"north", "south", "east", "west"}
-good_animals = ["rabbit","bird","bug"]
-bad_animals = ["wolf","poisonous snake"]
-health_status = ["Healthy","Injured","Poisoned"]
-food_status = ["Full","Hungry","Starving"]
-drink_status = ["Full","Thirsty","Dehydrating"]
-actions_options = {"wait","search","eat","drink"}
+good_animals = ["rabbit", "bird", "bug"]
+bad_animals = ["wolf", "poisonous snake"]
+health_status = ["Healthy", "Injured", "Poisoned"]
+food_status = ["Full", "Hungry", "Starving"]
+drink_status = ["Full", "Thirsty", "Dehydrating"]
+actions_options = {"wait", "search", "eat", "drink", "walk", "inventory"}
 
-inventory = ("watch", "wallet")
+inventory = ["watch", "wallet", "LG 430", "keys"]
 
 items = {"stone": 0.3,
          "branch": 0.2,
@@ -68,43 +68,49 @@ ap = 0
 #time.sleep (1)
 while turns > 0:
     print ("NEW TURN")
-    ap += 1
+    ap += 5
     #time.sleep (1)
     choice = raw_input("You have " + str(ap) + " action points.\n"
-                                               "What you want to do?(walk / actions)\n"
+                                               "What you want to do?(actions/skip)\n"
                                                "R:")
-    #Base walking loop
-    if choice == "walk":
-        path = wait_for_option(directions_options,"Which direction do you want to take? \n"
-                                                  "Possible directions: %s \n"
-                                                  "Your direction: ")
-        if path == "north":
-            print ("Walking.")
-            time.sleep(1)
-            print ("Walking..")
-            time.sleep(1)
-            print ("Walking...")
-            time.sleep(1)
-            turns -= 1
-            ap -= 1
-        else:
-            print ("I've wasted my time")
-    elif choice == "actions":
-        action = wait_for_option(actions_options,
-                                 "Which action do you want to take?\n"
-                                 "Possible actions: %s\n"
-                                 "Your action: ")
-        if action == "wait":
-            print ("I've waited some time and gain some energy")
-            ap += 1
-        if action == "search":
-            # Search action is defined
-            print ("Searching.")
-            time.sleep(1)
-            print ("Searching..")
-            time.sleep(1)
-            print ("Searching...")
-            time.sleep(1)
-            ap -= 1
-            new_item = np.random.choice(list(items.keys()), p=list(items.values()))
-            print ("You have found: " + str(new_item))
+    #Base pass turn loop
+    if choice == "skip":
+        print("You have skip the turn")
+
+    while ap > 0:
+        # Base walking loop
+        if choice == "actions":
+            action = wait_for_option(actions_options,
+                                     "You have " + str(ap) + " APs\n"
+                                     "Which action do you want to take?\n"
+                                     "Possible actions: %s\n"
+                                     "Your action: ")
+            if action == "inventory":
+                print ("My inventory has: " + str(inventory))
+            if action == "walk":
+                path = wait_for_option(directions_options, "Which direction do you want to take? \n"
+                                                           "Possible directions: %s \n"
+                                                           "Your direction: ")
+                if path == "north":
+                    print ("Walking.")
+                    time.sleep(1)
+                    print ("Walking..")
+                    time.sleep(1)
+                    print ("Walking...")
+                    time.sleep(1)
+                    ap -= 1
+            if action == "wait":
+                print ("I've waited some time and gain some energy")
+                ap += 1
+            if action == "search":
+                # Search action is defined
+                print ("Searching.")
+                time.sleep(1)
+                print ("Searching..")
+                time.sleep(1)
+                print ("Searching...")
+                time.sleep(1)
+                ap -= 1
+                new_item = np.random.choice(list(items.keys()), p=list(items.values()))
+                print ("You have found: " + str(new_item))
+                inventory.insert(0, str(new_item))
